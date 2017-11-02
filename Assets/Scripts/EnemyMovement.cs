05489c9f2b264b5519999e23ff01d68b706eda7e
Jeff Petriello <thebeff@gmail.com>
2017-11-02 16:22:11 -0400

    Setting up Enemy Movement
    establishing an initial random target position

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
+	Rigidbody2D rb;
+
+	float targetYMin;
+	public float targetYMax;
+	public float targetXMin;
+	public float targetXMax;
+
+	Vector3 currentPosition;
+	Vector3 targetPosition;
+
+	public float enemySpeed;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		rb = this.GetComponent<Rigidbody2D> ();
+
+		currentPosition = transform.position;
+		targetYMin = GameObject.Find ("GameManager").GetComponent<GameManager> ().safeZoneMax;
+
+		targetPosition = new Vector3 (Random.Range (targetXMin, targetXMax), Random.Range(targetYMin, targetYMax), 0);
+
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	public float backgroundScrollSpeed;
+	public float safeZoneMax;
 
 	private bool gameOver;
 
