3b26945381e4bd15f58373f97c4ce238eed29c44
Jeff Petriello <thebeff@gmail.com>
2017-11-02 15:02:21 -0400

    Fixed background scrolling
    fixed a bug when the background popped back up to the top without a value for newPosition

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -11,19 +11,26 @@ public class ScrollingObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		//fetch the background scroll speed
 		gameManager = GameObject.Find ("GameManager");
 		speed = gameManager.GetComponent<GameManager> ().backgroundScrollSpeed;
 
+		//create a copy of the scrolling object's current vertical position
 		newVerticalPosition = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//subtract the speed from that copy
 		newVerticalPosition -= speed;
 
+		//if the background falls below the game screen, put it back on top of it
+		//otherwise, set it to the new position
 		if (transform.position.y <= -17.88) {
 			transform.position = new Vector3 (0, 26.65f, 0);
+			newVerticalPosition = transform.position.y;
 		} else {
 			transform.position = new Vector3 (0, newVerticalPosition, 0);
 		}
