3213061bf7bc9d80de44c7824c7c59bd3eb1029b
Jeff Petriello <thebeff@gmail.com>
2017-11-02 15:08:58 -0400

    Added a Spawn Object
    and corrected the capitalization of enemy movement script

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyMovement : MonoBehaviour {
+public class SpawnerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
