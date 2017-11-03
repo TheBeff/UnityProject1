0289e415025e38c5ca281888143f92b92ce1a497
Jeff Petriello <thebeff@gmail.com>
2017-11-03 18:03:59 -0400

    Stuck on Glove Movement
    >: [

--- Assets/Scripts/GloveMovement.cs
+++ Assets/Scripts/GloveMovement.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class GloveMovement : MonoBehaviour {
 
+	Rigidbody2D rb;
+
+	private bool isMoving = false;
+
+	private Vector2 startPosition;
+	private Vector2 currentPosition;
+	private Vector2 newVelocity;
+
+	public float gloveSpeed;
 	// Use this for initialization
 	void Start () {
-		
+
+		rb = this.GetComponent<Rigidbody2D> ();
+		startPosition = new Vector2 (0, -3.6f);
+		currentPosition = rb.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+//		newVelocity = (startPosition - currentPosition).normalized * gloveSpeed;
+//
+//		if (!isMoving) {
+//			rb.velocity = newVelocity;
+//		}
+	}
+
+	void moveGlove(){
+		if(Input.GetKey(KeyCode.S)){
+			isMoving = true;
+			rb.velocity = new Vector2(0, -1 * gloveSpeed);
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			isMoving = true;
+			print ("ur pushing W");
+			rb.velocity = new Vector2(0, gloveSpeed);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			isMoving = true;
+			rb.velocity = new Vector2(-1 * gloveSpeed, 0);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			isMoving = true;
+			rb.velocity = new Vector2 (gloveSpeed, 0);
+		} else {
+			isMoving = false;
+		}
 	}
 }
