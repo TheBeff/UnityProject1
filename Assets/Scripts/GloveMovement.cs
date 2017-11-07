1296949076d31d73ce00afa0cdcdaffeff03fd7a
Jeff Petriello <thebeff@gmail.com>
2017-11-07 13:30:35 -0500

    Corrected glove movement

--- Assets/Scripts/GloveMovement.cs
+++ Assets/Scripts/GloveMovement.cs
@@ -8,8 +8,9 @@ public class GloveMovement : MonoBehaviour {
 
 	private bool isMoving = false;
 
+	public float jiggle_offset = 0.4f;
+
 	private Vector2 startPosition;
-	private Vector2 currentPosition;
 	private Vector2 newVelocity;
 
 	public float gloveSpeed;
@@ -17,42 +18,72 @@ public class GloveMovement : MonoBehaviour {
 	void Start () {
 
 		rb = this.GetComponent<Rigidbody2D> ();
-		startPosition = new Vector2 (0, -3.6f);
-		currentPosition = rb.position;
+		startPosition = this.transform.position;//new Vector2 (0, -3.6f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-//		newVelocity = (startPosition - currentPosition).normalized * gloveSpeed;
+		moveGlove ();
+
+//		newVelocity = 
 //
-//		if (!isMoving) {
+//		if (!isMoving && rb.position.y > -3.6f) {
 //			rb.velocity = newVelocity;
 //		}
+
 	}
 
 	void moveGlove(){
+
+		Vector2 newVelocity = new Vector2 (0, 0);
+
+		isMoving = false;
+
 		if(Input.GetKey(KeyCode.S)){
 			isMoving = true;
-			rb.velocity = new Vector2(0, -1 * gloveSpeed);
+			print ("ur pushing S");
+			newVelocity += new Vector2(0, -1 * gloveSpeed);
 		}
-
 		if(Input.GetKey(KeyCode.W)){
 			isMoving = true;
 			print ("ur pushing W");
-			rb.velocity = new Vector2(0, gloveSpeed);
+			newVelocity += new Vector2(0, gloveSpeed);
 		}
 
 		if(Input.GetKey(KeyCode.A)){
 			isMoving = true;
-			rb.velocity = new Vector2(-1 * gloveSpeed, 0);
+			newVelocity += new Vector2(-1 * gloveSpeed, 0);
 		}
 
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey(KeyCode.D)) {
 			isMoving = true;
-			rb.velocity = new Vector2 (gloveSpeed, 0);
-		} else {
-			isMoving = false;
+			newVelocity += new Vector2 (gloveSpeed, 0);
+		} 
+
+		rb.velocity = newVelocity;
+
+		if (!isMoving) {
+			resetGlove ();
 		}
 	}
+
+
+	void resetGlove(){
+		if (rb.position != startPosition) {
+			Debug.Log ("trying to reset position");
+			Vector2 newVelocity = new Vector2 (0, 0);
+			if (rb.position.x < startPosition.x - jiggle_offset) 
+				newVelocity += new Vector2(gloveSpeed, 0);
+			if (rb.position.x > startPosition.x + jiggle_offset) 
+				newVelocity += new Vector2(-gloveSpeed, 0);
+			if (rb.position.y < startPosition.y - jiggle_offset) 
+				newVelocity += new Vector2(0, gloveSpeed);
+			if (rb.position.y > startPosition.y + jiggle_offset) 
+				newVelocity += new Vector2(0, -gloveSpeed);
+
+			rb.velocity = newVelocity;
+		}
+	}
+
 }
