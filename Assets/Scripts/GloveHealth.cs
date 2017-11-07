b383f7a813dd98232805bb8b77142b1dbe931c26
Jeff Petriello <thebeff@gmail.com>
2017-11-07 13:50:34 -0500

    Created Glove Health Script
    Triggers gameOver on 0

---
+++ Assets/Scripts/GloveHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveHealth : MonoBehaviour {
+
+	public int health;
+	bool gameOver;
+	// Use this for initialization
+	void Start () {
+		gameOver = GameObject.Find ("GameManager").GetComponent<GameManager>().gameOver;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			gameOver = true;
+		}
+	}
+}
