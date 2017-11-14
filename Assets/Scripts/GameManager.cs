85cc1471aecae036576afa559894e017808b69bf
Jeff Petriello <thebeff@gmail.com>
2017-11-14 01:07:40 -0500

    Added UI for health and level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -17,16 +18,33 @@ public class GameManager : MonoBehaviour {
 	AudioSource soundtrack;
 	AudioSource theme;
 
+	private GameObject score;
+	private Text scoreText;
+	private SpawnerScript spawner;
+
+	private GameObject health;
+	private Text healthText;
+	private GloveHealth gloveHealth;
+
 
 	// Use this for initialization
 	void Start () {
 		intro = true;
 		gameStarted = false;
 		gameOver = false;
+
 		musicTracks = GetComponents<AudioSource> ();
 		soundtrack = musicTracks[0];
 		theme = musicTracks [1];
 		introMusicPlayed = false;
+
+		score = GameObject.Find ("Score");
+		scoreText = score.GetComponent<Text> ();
+		spawner = GameObject.Find ("EnemySpawner").GetComponent<SpawnerScript> ();
+		health = GameObject.Find ("Health");
+		healthText = health.GetComponent<Text> ();
+		gloveHealth = GameObject.Find ("glove").GetComponent<GloveHealth> ();
+
 	}
 	
 	// Update is called once per frame
@@ -45,5 +63,9 @@ public class GameManager : MonoBehaviour {
 
 		if (gameOver)
 			SceneManager.LoadScene ("GameOver");
+
+		scoreText.text = "BOB LEVEL: " + (spawner.level + 1);
+		healthText.text = "FREDDIE HP: " + (gloveHealth.health);
+
 	}
 }
