e491f916da5ac1f72ef20c7abfb67e45f4ee26b5
Jeff Petriello <thebeff@gmail.com>
2017-11-13 23:36:56 -0500

    Added a Game Over Scene
    Activated when glove's health is 0

---
+++ Assets/GameOverManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+	//variables needed for making sure the user types two 'r's to reset
+	float maxDelay = 0.5f;
+	bool resetReady = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("r")){
+			if (resetReady)
+				Reset ();
+			else
+				PrepareReset (true);
+		}
+	}
+
+	void Reset(){
+		resetReady = false;
+		SceneManager.LoadScene ("ProjectGreenGlove");
+	}
+
+	void PrepareReset(bool getReady){
+		CancelInvoke ("CancelReset");
+		Invoke ("CancelReset", maxDelay);
+		resetReady = true;
+	}
+
+	void CancelInvoke(){
+		resetReady = false;
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -27,6 +28,8 @@ public class GameManager : MonoBehaviour {
 			gameStarted = false;
 			soundtrack.Play ();
 		}
-			
+
+		if (gameOver)
+			SceneManager.LoadScene ("GameOver");
 	}
 }
